952648038dc88be7ddfcf22fae162ab877e40d59
JiaJun Yim <yimjiajun@icloud.com>
2023-01-20 18:33:50 +0800

    nake version
    Signed-off-by: JiaJun Yim <yimjiajun@icloud.com>

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Zephyr Application Debian package Installation
+
+## Installation
+
+install Debian package for zephyr application
+
+	./debian_package.sh install
+
+*for interactive `./debian_package.sh`*
+
+## Download Zephyr Application Projects
+
+download zephyr application projects
+
+	eczephyr install ${PATH_TO_DOWNLOAD}
+
+*`PATH_TO_DOWNLOAD`: path to download zephyr app project*
+
+## Build Zephyr Application Projects
+
+build zephyr application project
+
+	eczephyr build ${APP_PROJECT_PATH}
+
+*`APP_PROJECT_PATH`: should target to **ecfw_zephyr** dir*
+
+build with supporting board
+
+	eczephyr build -b -c ${APP_PROJECT_PATH}
+
+*`-b`: pop up to boards selection*
+*`-c`: clear app project build dir*
+
+more details:
+
+	eczephyr build -h
+
+## Uninstall
+
+uninstall zephyr application Debian package
+
+	./debian_package.sh uninstall
--- debian_package.sh
+++ debian_package.sh
@@ -4,7 +4,7 @@ package_name="eczephyr"
 prj_package_dir_name="zephyr_debian_package"
 prj_path="$(dirname $(readlink -f "$0"))"
 commands=('create' 'install' 'uninstall')
-cmd_sel=
+cmd_sel="$1"
 
 function debian_pacakge_actions_select {
 	local max_cmd_sup=${#commands[*]}
@@ -19,13 +19,14 @@ function debian_pacakge_actions_select {
 		for((i = 0; i < ${max_cmd_sup}; i++)); do
 			printf "%2d | %s \n" "$i" "${commands[$i]}"
 		done
-		read -t 20 -p "Select action or 'q' to quit [0-${max_cmd_idx}|q] : "
+		read -t 20 -p "Enter action or 'q' to quit [0-${max_cmd_idx}|q] : "
 		if [ ! -z $REPLY ]; then
+			[ $REPLY = 'q' ] &&\
+				exit 0
+
 			[[ ($REPLY =~ ^[0-9]+$) ]] && [ $REPLY -lt ${max_cmd_sup} ] &&\
 				cmd_sel="${commands[$REPLY]}"
 
-			[ $REPLY = 'q' ] &&\
-				exit 0
 		fi
 	done
 
@@ -39,7 +40,7 @@ function debian_pacakge_cmd_process {
 			;;
 		'install')
 			[ ! -f "${prj_path}/${prj_package_dir_name}.deb" ] &&\
-				$(echo -e "\033[1;31m'$1' selected command is unrecognized\033[0m" >&2;\
+				$(echo -e "\033[1;31m error\033[0m: debian package is not found!" >&2;\
 				exit 1)
 			sudo dpkg -i "${prj_path}/${prj_package_dir_name}.deb"
 			;;
@@ -47,7 +48,7 @@ function debian_pacakge_cmd_process {
 			sudo dpkg -P "${package_name}"
 			;;
 		*)
-			echo -e "\033[1;31m'$1' selected command is unrecognized\033[0m" >&2
+			echo -e "\033[1;31m error\033[0m:'$1' selected command is unrecognized" >&2
 			exit 1
 			;;
 	esac
@@ -58,6 +59,9 @@ function debian_pacakge_cmd_process {
 while (true); do
 	debian_pacakge_actions_select; [ "$?" -gt 0 ] && exit 1
 	debian_pacakge_cmd_process; [ "$?" -gt 0 ] && exit 1
+	[ ! -z "$1" ] && break
 	cmd_sel=
 	sleep 0.5
 done
+
+exit 0
